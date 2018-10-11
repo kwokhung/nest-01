4d708f88a32b16204f6421732f43c25e8cf724af
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-10-11 11:35:19 +0800

    init

--- src/controllers/expense/expense.controller.ts
+++ src/controllers/expense/expense.controller.ts
@@ -10,45 +10,43 @@ class Expense {
     selected: boolean;
 }
 
+class MockData {
+
+    public static Expenses: Expense[] = [
+        {
+            'id': 1,
+            'applicationDate': '20181001',
+            'applicationNo': 'App-01',
+            'payee': '16990',
+            'status': '',
+            'selected': false
+        },
+        {
+            'id': 2,
+            'applicationDate': '20181002',
+            'applicationNo': 'App-02',
+            'payee': '59990',
+            'status': 'Exported',
+            'selected': false
+        },
+        {
+            'id': 3,
+            'applicationDate': '20181003',
+            'applicationNo': 'App-03',
+            'payee': 'abc',
+            'status': '',
+            'selected': false
+        }
+    ];
+
+}
+
 @Controller('expense')
 export class ExpenseController {
 
-    @Get('/getExpenses')
-    getExpenses(): Observable<Expense[]> {
-        return of([
-            {
-                'id': 1,
-                'applicationDate': '20181001',
-                'applicationNo': 'App-01',
-                'payee': '16990',
-                'status': '',
-                'selected': false
-            },
-            {
-                'id': 2,
-                'applicationDate': '20181002',
-                'applicationNo': 'App-02',
-                'payee': '59990',
-                'status': 'Exported',
-                'selected': false
-            },
-            {
-                'id': 3,
-                'applicationDate': '20181003',
-                'applicationNo': 'App-03',
-                'payee': 'abc',
-                'status': '',
-                'selected': false
-            },
-            {
-                'id': 4,
-                'applicationDate': '20181004',
-                'applicationNo': 'App-04',
-                'payee': 'abc',
-                'status': 'Exported',
-                'selected': false
-            }
-        ]);
+    @Get('/getAllExpenses')
+    getAllExpenses(): Observable<Expense[]> {
+        return of(MockData.Expenses);
     }
 
 }
